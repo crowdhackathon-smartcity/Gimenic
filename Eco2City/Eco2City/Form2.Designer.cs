6c8bb9a07f0701643ee389a7682a09e6c60324d7
inkorect <minasdioletis@gmail.com>
2017-05-13 14:37:30 +0300

    version7
    +goog.maps

--- Eco2City/Eco2City/Form2.Designer.cs
+++ Eco2City/Eco2City/Form2.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.cb1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // cb1
@@ -43,20 +44,31 @@
             "Κορυδαλλός",
             "Ρέντη",
             "Ελευσίνα"});
-            this.cb1.Location = new System.Drawing.Point(642, 456);
+            this.cb1.Location = new System.Drawing.Point(676, 612);
             this.cb1.Name = "cb1";
-            this.cb1.Size = new System.Drawing.Size(188, 21);
+            this.cb1.Size = new System.Drawing.Size(269, 21);
             this.cb1.TabIndex = 0;
             this.cb1.DropDownClosed += new System.EventHandler(this.cb1_DropDownClosed);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(200, 28);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1204, 473);
+            this.webBrowser1.TabIndex = 1;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1491, 777);
+            this.ClientSize = new System.Drawing.Size(1572, 777);
+            this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.cb1);
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "EcoCIty";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
@@ -65,5 +77,6 @@
         #endregion
 
         private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
--- Eco2City/Eco2City/Form2.cs
+++ Eco2City/Eco2City/Form2.cs
@@ -12,6 +12,7 @@ namespace Eco2City
 {
     public partial class Form2 : Form
     {
+        public string x;
         public Form2()
         {
             InitializeComponent();
@@ -22,8 +23,60 @@ namespace Eco2City
 
         private void cb1_DropDownClosed(object sender, EventArgs e)
         {
-            string x = cb1.SelectedItem.ToString();
-            MessageBox.Show(x);
+             x = cb1.SelectedItem.ToString();
+
+
+            if (x=="Καλλίπολη")
+            {
+                {
+
+                   
+
+                    
+                    try
+
+                    {
+
+                        StringBuilder queryadress = new StringBuilder();
+
+                        queryadress.Append("https://www.google.gr/maps/place/Kallipoli,+Pireas+185+39/@37.9307333,23.633287,17z/data=!3m1!4b1!4m5!3m4!1s0x14a1b963aac49e3f:0x7c2e3e6cfb4efbc8!8m2!3d37.9307333!4d23.6354757?hl=en");
+
+
+                        webBrowser1.Navigate(queryadress.ToString());
+
+                    }
+
+
+
+                    catch (Exception ex)
+
+                    {
+
+                        MessageBox.Show(ex.Message.ToString(), "Erro");
+
+                    }
+
+                }
+
+            }
+            else if (x=="Νίκαια")
+            {
+
+            }
+            else if (x=="Κορυδαλλός")
+            {
+
+            }
+
+            else if (x=="Ρέντη")
+            {
+
+            }
+
+            else if (x=="Ελευσίνα")
+            {
+
+            }
         }
     }
 }
