32b9e9594f44cdf742d4610e71ab5f9b4d9fd28b
inkorect <minasdioletis@gmail.com>
2017-05-13 11:02:15 +0300

    ******
    Prwth fora

---
+++ Eco2City/Eco2City/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Eco2City
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.geiaTimer = new System.Windows.Forms.Timer(this.components);
+            this.KalwsorTimer = new System.Windows.Forms.Timer(this.components);
+            this.geia = new System.Windows.Forms.Label();
+            this.kalwsorisma = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // geiaTimer
+            // 
+            this.geiaTimer.Interval = 19;
+            this.geiaTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // KalwsorTimer
+            // 
+            this.KalwsorTimer.Interval = 19;
+            this.KalwsorTimer.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // geia
+            // 
+            this.geia.AutoSize = true;
+            this.geia.Font = new System.Drawing.Font("Georgia", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.geia.Location = new System.Drawing.Point(775, 308);
+            this.geia.Name = "geia";
+            this.geia.Size = new System.Drawing.Size(306, 72);
+            this.geia.TabIndex = 0;
+            this.geia.Text = "Γεια σας !";
+            // 
+            // kalwsorisma
+            // 
+            this.kalwsorisma.AutoSize = true;
+            this.kalwsorisma.Font = new System.Drawing.Font("Georgia", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.kalwsorisma.Location = new System.Drawing.Point(393, 317);
+            this.kalwsorisma.Name = "kalwsorisma";
+            this.kalwsorisma.Size = new System.Drawing.Size(1148, 72);
+            this.kalwsorisma.TabIndex = 1;
+            this.kalwsorisma.Text = "Καλως ορίσατε στην εφαρμογή EcoCity";
+            this.kalwsorisma.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(807, 73);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(802, 697);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1882, 839);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.kalwsorisma);
+            this.Controls.Add(this.geia);
+            this.Name = "Form1";
+            this.Text = "EcoCity";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer geiaTimer;
+        private System.Windows.Forms.Timer KalwsorTimer;
+        private System.Windows.Forms.Label geia;
+        private System.Windows.Forms.Label kalwsorisma;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Eco2City/Eco2City/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Eco2City
+{
+    public partial class Form1 : Form
+    {
+        bool fadeflag = false;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            geia.Visible = true;
+            kalwsorisma.Visible = false;
+            geia.ForeColor = this.BackColor;
+            kalwsorisma.ForeColor = this.BackColor;
+            geiaTimer.Enabled=true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int fadingSpeed = 3;
+            if (!fadeflag)
+            {
+                kalwsorisma.ForeColor = Color.FromArgb(kalwsorisma.ForeColor.R - fadingSpeed, kalwsorisma.ForeColor.G - fadingSpeed, kalwsorisma.ForeColor.B - fadingSpeed);
+            }
+            if (kalwsorisma.ForeColor.R == 0) fadeflag = true;
+
+            if (fadeflag)
+            {
+                kalwsorisma.ForeColor = Color.FromArgb(kalwsorisma.ForeColor.R + fadingSpeed, kalwsorisma.ForeColor.G + fadingSpeed, kalwsorisma.ForeColor.B + fadingSpeed);
+            }
+            if (kalwsorisma.ForeColor.R == this.BackColor.R)
+            {
+                fadeflag = false;
+                kalwsorisma.Visible = false;
+            }
+
+            }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int fadingSpeed = 3;
+            if (!fadeflag)
+            {
+                geia.ForeColor = Color.FromArgb(geia.ForeColor.R - fadingSpeed, geia.ForeColor.G - fadingSpeed, geia.ForeColor.B - fadingSpeed);
+            }
+            if (geia.ForeColor.R == 0) fadeflag = true;
+
+            if (fadeflag)
+            {
+                geia.ForeColor = Color.FromArgb(geia.ForeColor.R + fadingSpeed, geia.ForeColor.G + fadingSpeed, geia.ForeColor.B + fadingSpeed);
+            }
+            if (geia.ForeColor.R == this.BackColor.R)
+            {
+                geiaTimer.Stop();
+                fadeflag = false;
+                geia.Visible = false;
+                kalwsorisma.Visible = true;
+                KalwsorTimer.Start();
+            }
+        }
+    }
+}
